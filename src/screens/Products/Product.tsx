dae24277631d66adec1e09731ad33cc727d58bc4
Popescu Radu <radu.p1995@yahoo.com>
2023-02-23 21:41:33 +0200

    Product Screen improved arhitecture

--- src/screens/Products/Product.tsx
+++ src/screens/Products/Product.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
-import ProductsHeader from './components/ProductsHeader/ProductsHeader';
-import ProductsTable from './components/ProductsTable/ProductsTable';
+import ProductsHeader from '../../components/ProductsHeader/ProductsHeader';
+import ProductsTable from '../../components/ProductsTable/ProductsTable';
 import NewProduct from './components/NewProduct/NewProduct';
-import { getProducts } from 'src/api/getProducts';
 import useMockup from 'src/hooks/useMockup';
 
 const Product = () => {
   const [addProduct, setAddProduct] = useState<boolean>(false);
-  const { data, product, isLoading, setProduct, setData, setLoading } = useMockup();
-  // const [isLoading, setLoading] = useState<boolean>(true);
-  // const [data, setData] = useState<[]>([]);
-  // const [product, setProduct] = useState<{
-  //   id?: number;
-  //   title?: string;
-  //   price?: string;
-  //   cathegory?: string;
-  //   description?: string;
-  //   image?: string;
-  // }>({});
+  const { data, product, isLoading, setProduct } = useMockup();
 
   const hanldeAddProduct = (id?: number): void => {
     if (id) {
@@ -28,11 +17,6 @@ const Product = () => {
     }
     setAddProduct(!addProduct);
   };
-
-  // useEffect(() => {
-  //   getProducts(setData, setLoading, setProduct);
-
-  // }, []);
 
   if (addProduct) {
     return (
